65b502f929a3b83d637e90dc63b697547638d42b
Jonathen Colon <dev@naberusgames.com>
2024-02-10 23:45:17 -0500

    added node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@ import { defineConfig } from 'astro/config';
 
 // import cloudflare from "@astrojs/cloudflare";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-  // output: "server",
-  // adapter: cloudflare()
-});
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+} // output: "server",
+// adapter: cloudflare()
+);
