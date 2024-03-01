5b3e572e85bc5487baf1506a585ad0395a47b74e
Jonathen Colon <dev@naberusgames.com>
2024-02-29 19:50:51 -0500

    fixing for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-// import cloudflare from "@astrojs/cloudflare";
-
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
-} // output: "server",
-// adapter: cloudflare()
-);
+  adapter: cloudflare()
+});
